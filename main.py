ef8cbbc0980b162e8e4864f31a0f242ad33ccb83
Linglai Chen <linglai27@yahoo.com>
2022-06-23 14:40:05 -0400

    render explosion with appropriate colors

--- main.py
+++ main.py
@@ -2,20 +2,47 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-import math
+import random
+import time
+import matplotlib.pyplot as plt
 
 import output
+import rtweekend
 from light import Light
+from perlin_noise import PerlinNoise3D
 from ray import Ray
 from sphere import Sphere
 from vec3 import Color, Point3, Vec3
 
+
+def palette_fire(d: float):
+    yellow = Color(1.7, 1.3, 1.0)
+    orange = Color(1, 0.6, 0)
+    red = Color(1, 0, 0)
+    darkgray = Color(0.2, 0.2, 0.2)
+    gray = Color(0.4, 0.4, 0.4)
+
+    x = rtweekend.clamp(d, 0, 1)
+
+    if x < 0.25:
+        return Vec3.linear_interpolation(gray, darkgray, 4 * x)
+    elif x < 0.5:
+        return Vec3.linear_interpolation(darkgray, red, 4 * x - 1)
+    elif x < 0.75:
+        return Vec3.linear_interpolation(red, orange, 4 * x - 2)
+    else:
+        return Vec3.linear_interpolation(orange, yellow, 4 * x - 3)
+
+
 if __name__ == '__main__':
+
+    # Start Timing
+    start = time.perf_counter()
+
     # Image
 
-    aspect_ratio = 16 / 9
-    image_width = 400
+    aspect_ratio = 1
+    image_width = 384
     image_height = int(image_width / aspect_ratio)
 
     # Camera
@@ -31,19 +58,27 @@ if __name__ == '__main__':
 
     # Lights
 
-    light1 = Light(Vec3(0.5, 0.4, 1.0), Point3(2, 2, -1))
-    light2 = Light(Vec3(0.3, 0.7, 0.9), Point3(-5, -2, -1))
-    lights = [light2]
+    light1 = Light(Vec3(1, 1, 1), Point3(10, 10, 10))
+    lights = [light1]
 
     # Objects
 
-    sphere_1 = Sphere(1 / math.sqrt(2), Point3(0, 0, -1), Color(1.0, 1.0, 1.0))
+    sphere_1 = Sphere(1.5, Point3(0, 0, -3))
 
-    output_file = open("output.ppm", "w+")
+    # noise attribute set up
+    noise_amplitude = 1
+
+    # Rendering
+
+    output_file = open("output2.ppm", "w+")
     begin_txt = "P3\n{0} {1} \n255\n".format(image_width, image_height)
-    print(begin_txt)
+    output_file.write(begin_txt)
+
+    # Date Recording
+    noise_stat = []
 
     for j in range(image_height - 1, - 1, - 1):
+
         for i in range(image_width):
             u = i / (image_width - 1)
             v = j / (image_height - 1)
@@ -53,12 +88,22 @@ if __name__ == '__main__':
 
             samples_per_pixel = 1
 
-            tmp = sphere_1.hit(r_in, 0, math.inf, lights)
+            tmp = sphere_1.sphere_trace(r_in, 0, lights)
             if tmp is not None and len(tmp) == 4:
-                noise = math.sin(1200 * tmp[1].x()) * math.cos(480 * tmp[1].y()) * math.sin(720 * tmp[1].z())
-                pixel_color = sphere_1.color * tmp[3]
+                noise_level = (sphere_1.radius - (tmp[1] - sphere_1.center).length()) / noise_amplitude
+                noise_stat.append(noise_level)
+                pixel_color = palette_fire((noise_level + 0.25) * 2) * tmp[3]
             output.write_color(output_file, pixel_color, samples_per_pixel)
 
+        print("{0} percent done".format(int((image_height - j) / image_height * 100)))
+
     output_file.close()
+    plt.hist(noise_stat)
+    plt.show()
+
+    # End Timing
+    end = time.perf_counter()
+
+    print(end - start)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- sphere.py
+++ sphere.py
@@ -1,34 +1,35 @@
 import math
 
 from light import Light
+from perlin_noise import PerlinNoise3D
 from ray import Ray
 from rigidbody import RigidBody
+import noise
 from vec3 import Vec3, Color, Point3
+
+perlin_noise_3D = PerlinNoise3D(256)
 
 
 class Sphere(RigidBody):
 
-    def __init__(self, r, c, col=Color(1, 1, 1)):
+    def __init__(self, r, c):
         self.radius = r
         self.center = c
-        self.color = col
 
-    def hit(self, r_in: Ray, tmin, tmax, lights=None):
+    def sphere_trace(self, r_in: Ray, tmin, lights=None):
         cur = tmin
 
         r_in.direction = r_in.direction.unit_vector()
-        if Vec3.dot(r_in.origin - self.center, r_in.origin - self.center) - self.radius ** 2 > 0:
-            tmax_possible = math.sqrt(Vec3.dot(r_in.origin - self.center, r_in.origin - self.center) - self.radius ** 2)
-            tmax = min(tmax, tmax_possible)
 
         encounter = False
 
-        while cur <= tmax:
-            if self.signed_distance_function(r_in.at(cur)) <= 1e-8:
+        for _ in range(128):
+            d = self.signed_distance_function(r_in.at(cur))
+            if d <= 0:
                 encounter = True
                 break
             else:
-                cur += 1e-2
+                cur += max(1e-2, 0.1 * d)
 
         if not encounter:
             return None
@@ -39,17 +40,20 @@ class Sphere(RigidBody):
         if not lights:
             return [cur, intersection, normal]
 
-        light_intensity = Color(.3, 0.3, 0.2)
+        light_intensity = Color(0, 0, 0)
         for light in lights:
-            light_intensity += light.intensity.scalar_multiply(max(0, Vec3.dot(normal, (light.position - intersection).unit_vector())))
+            light_intensity += light.intensity.scalar_multiply(
+                max(0.4, Vec3.dot(normal, (light.position - intersection).unit_vector())))
 
         return [cur, intersection, normal, light_intensity]
 
     def signed_distance_function(self, pt):
-        return math.sqrt(Vec3.dot(pt - self.center, pt - self.center)) - self.radius - Sphere.sine_noise(pt)
+        pt2 = pt - self.center
+        displacement = - perlin_noise_3D.fractal_brownian_motion(pt2.scalar_multiply(3.4))
+        return math.sqrt(Vec3.dot(pt2, pt2)) - (self.radius + displacement)
 
     def compute_normal(self, pt: Point3):
-        eps = 1e-4
+        eps = 1e-1
 
         tmp = self.signed_distance_function(pt)
         dx = self.signed_distance_function(pt + Vec3(eps, 0, 0)) - tmp
